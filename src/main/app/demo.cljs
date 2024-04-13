31f7dfab19fa7e7198bdb0eebb26b7684a4fd04b
Natasha Hill-Tout <tashht2@gmail.com>
2024-04-13 23:41:54 +0100

    delete

--- src/main/app/demo.cljs
+++ src/main/app/demo.cljs
@@ -5,9 +5,22 @@
     [clojure.pprint :refer [pprint]]))
 
 (defonce state (atom {:count1 0
-                      :hello  "world"
-                      :champs [{:name "Foo"}
-                               {:name "Bar"}]}))
+                      :champion [{{:name "Aatrox"
+                                   :class "Bruiser"}
+                                  {:name "Ahri"
+                                   :class "Utility Assassin"}
+                                  {:name "Akali"
+                                   :class "Assassin"}
+                                  {:name "Akshan"
+                                   :class "Assassin"}
+                                  {:name "Alistar"
+                                   :class "Tank"}
+                                  {:name "Amumu"
+                                   :class "Tank"}
+                                  {:name "Anivia"
+                                   :class "Control Mage"}
+                                  {:name "Annie"
+                                   :class "Burst Utility Mage"}}]}))
 
 (defn ui-app-state [current-state]
   ;; HTM
@@ -20,20 +33,37 @@
         (pprint current-state)))
     (dom/hr {})))
 
+
+
 (defn Root []
   (let [current-state @state
         current-count (get current-state :count1)]
-    (dom/div {:style {:color "red"}}
+    (dom/div {:style {:color "dark green"}}
+      "Champion Selected"
       (dom/button
         {:onClick (fn [_]
-                    (swap! state update :champs conj {:name (str "Champion" current-count)})
+                    (swap! state update :champion conj {:name (str "Champion" current-count)})
                     (swap! state update :count1 inc))}
         (str "Clicked:" current-count))
+      (dom/button
+        {:onClick (fn [_]
+                    (swap! state update :champion #(sort-by :class %)))}
+        (str "Class Sort:"))
+      (dom/button
+        {:onClick (fn [_]
+                    (swap! state update :champion #(sort-by :name %)))}
+        (str "Name Sort:"))
+
+
+
       (map
-        (fn [champ]
-          (dom/div {} (get champ :name)))
-        (get current-state :champs))
+        (fn [champion]
+          (dom/div {} (get champion :name)))
+        (get current-state :champion))
       (ui-app-state current-state))))
+
+
+
 
 ;; HTML: <button onClick=... >Label</button>
 ;; (dom/button {:onClick (fn [] ..)} "Label")
